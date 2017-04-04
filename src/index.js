f5003aabc65ecb3a3b291dfa978eb4c8117ee64f
Joshua Jacobson <joshua.jacobson@tapingo.com>
2017-04-04 11:16:10 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import hyperHTML from 'hyperhtml';
+
+// this is hyperHTML
+function tick(render) {
+  render`
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
+    </div>
+  `;
+}
+
+setInterval(tick, 1000,
+  hyperHTML.bind(document.getElementById('root'))
+);
